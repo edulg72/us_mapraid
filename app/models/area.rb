779d5244a6d5eb7a3672beca380323477cdda9f0
Eduardo Leão Garcia <edulg72@gmail.com>
2016-05-20 16:13:33 -0300

    Fixed area filter in area model.

--- app/models/area.rb
+++ app/models/area.rb
@@ -4,7 +4,7 @@ class Area < ActiveRecord::Base
   has_many :cities, class_name: 'CityMapraid'
   has_many :segments, through: :cities
 
-  scope :mapraid, -> {where(id: 1814)}
+  scope :mapraid, -> {where(id: 1812)}
   scope :others, -> {where('id not in (1,2,3)')}
   scope :states, -> {where('id in (4,5)')}
 end
